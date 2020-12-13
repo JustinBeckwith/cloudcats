f32df95b067fedda3496d5bca62427fa3463c156
Justin Beckwith <justin.beckwith@gmail.com>
2020-12-12 17:00:54 -0800

    more

--- web/server.js
+++ web/server.js
@@ -8,28 +8,21 @@ const express = require('express');
 const relay = require('./catrelay');
 
 const app = express();
-const http = require('http').createServer(app);
-const port = process.env.PORT || 8080;
-
-// Set up socket.io
-const io = require('socket.io')(http, {
-	transports: ['polling']
-});
-
-// Configure jade views
 app.set('view engine', 'pug');
-
-// Set up static public handler
 app.use(express.static('public'));
-
 app.get('/', (request, response) => {
 	response.render('index');
 });
 
-// Start the server
+const http = require('http').createServer(app);
+const port = process.env.PORT || 8080;
+
+const io = require('socket.io')(http, {
+	transports: ['polling']
+});
+
 http.listen(port, () => {
 	console.log(`cloudcats web listening on ${port}`);
 });
 
-// Start listening for cats
 relay.listen(io);
